6fd17ea23d3593322006c77719f4a098483dfed8
juyss <1028507471@qq.com>
2020-09-13 18:33:20 +0800

    add project

---
+++ src/main/java/com/juyss/mapper/BookMapper.java
@@ -0,0 +1,13 @@
+package com.juyss.mapper;
+
+/**
+ * @author ShmeBluk
+ * @version 1.0
+ * @ClassName: BookMapper
+ * @Desc: book表的数据库操作接口
+ * @package com.juyss.mapper
+ * @project SSM-Merge
+ * @date 2020/9/13 16:02
+ */
+public interface BookMapper {
+}
---
+++ src/main/java/com/juyss/service/BookService.java
@@ -0,0 +1,13 @@
+package com.juyss.service;
+
+/**
+ * @author ShmeBluk
+ * @version 1.0
+ * @ClassName: BookService
+ * @Desc:
+ * @package com.juyss.service
+ * @project SSM-Merge
+ * @date 2020/9/13 17:36
+ */
+public interface BookService {
+}
